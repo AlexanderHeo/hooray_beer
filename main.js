e018bc112e4cd53377de243aec366ff74fc6f894
Alexander Heo <jikster712@gmail.com>
2021-03-09 19:46:35 -0800

    creates and destroys modal on click

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ function initializeApp() {
   getBeerList()
 }
 
+function addClickHandlers() {
+  $('#footerPlusButton').on('click', footerButtonClicked)
+}
+
 async function getBeerList() {
   const response = await fetch('https://hooraybeer-d468f-default-rtdb.firebaseio.com/beers.json', {
     method: 'GET',
@@ -17,17 +21,9 @@ async function getBeerList() {
   if (database) setupBeerTable()
 }
 
-function addClickHandlers() {
-  $('#footerPlusButton').on('click', footerButtonClicked)
-}
-
 function footerButtonClicked() {
-  openModal()
   toggleButton()
-}
-
-function openModal() {
-  $('#addModal').toggleClass('hide')
+  toggleModal()
 }
 
 function toggleButton() {
@@ -36,6 +32,17 @@ function toggleButton() {
     button.text('CLOSE')
   } else {
     button.text('ADD')
+  }
+}
+
+function toggleModal() {
+  const addModal = $('#addModal')
+  addModal.toggleClass('hide')
+  const classList = $('#addModal').attr('class')
+  if (classList.split(' ')[1] === 'hide') {
+    takedownAddModal()
+  } else {
+    setupAddModal()
   }
 }
 
@@ -59,4 +66,51 @@ function setupBeerTable() {
       $('#table-body').append(row)
     }
   })
+}
+
+function takedownAddModal() {
+  // console.log('take down')
+  $('#addModal').empty()
+}
+
+function setupAddModal() {
+  // console.log('setup')
+  const div = $('<div>')
+  $('#addModal').append(div)
+  const col = $('<div>', { class: 'col-lg-4 col-md-12 col-sm-12 formContainer', id: 'addModalForm' })
+  const form = $('<form>')
+  const h4 = $('<h4>').text('Add a New Beer')
+  const formSectionBeer = $('<div>', { class: 'form-section' })
+  const formSectionBrewery = $('<div>', { class: 'form-section' })
+  const formSectionRating = $('<div>', { class: 'form-section' })
+  const formSectionTasting = $('<div>', { class: 'form-section' })
+  const labelBeer = $('<label>').text('Beer:')
+  const inputBeer = $('<input>')
+  const labelBrewery = $('<label>').text('Brewery:')
+  const inputBrewery = $('<input>')
+  const labelRating = $('<label>').text('Rating:')
+  const inputRating = $('<input>')
+  const labelTasting = $('<label>').text('Tasting:')
+  const inputTasting = $('<textarea>')
+  const buttons = $('<div>', { class: 'button-container' })
+  const buttonAdd = $('<button>', { type: 'button', class: 'btn btn-primary' }).text('ADD')
+  const buttonCancel = $('<button>', { type: 'button', class: 'btn btn-secondary' }).text('CANCEL')
+  formSectionBeer.append(labelBeer)
+  formSectionBeer.append(inputBeer)
+  formSectionBrewery.append(labelBrewery)
+  formSectionBrewery.append(inputBrewery)
+  formSectionRating.append(labelRating)
+  formSectionRating.append(inputRating)
+  formSectionTasting.append(labelTasting)
+  formSectionTasting.append(inputTasting)
+  buttons.append(buttonAdd)
+  buttons.append(buttonCancel)
+  form.append(h4)
+  form.append(formSectionBeer)
+  form.append(formSectionBrewery)
+  form.append(formSectionRating)
+  form.append(formSectionTasting)
+  form.append(buttons)
+  col.append(form)
+  $('#addModal').append(col)
 }
